ef3e646a2c2c87553898bf590442182ba852bc7e
JichengYuan <yuan_jcc@163.com>
2026-06-01 15:50:17 +0800

    Update attention.py

--- models/cdi/attention.py
+++ models/cdi/attention.py
@@ -12,10 +12,11 @@ class SelfAttention(nn.Module):
     def forward(self, x):
         b, c = x.shape  # b: batch_size, c: embed_size
         query = self.query(x).view(b, 1, c)  # bs x 1 x dim
-        key = self.key(x).view(b, 1, c)  # bs x 1 x dim
+        key = self.key(x).view(b, 1, c)      # bs x 1 x dim
         value = self.value(x).view(b, 1, c)  # bs x 1 x dim
 
         scores = torch.bmm(query, key.transpose(1, 2)) / torch.sqrt(torch.tensor(c, dtype=torch.float32))  # bs x 1 x 1
         attention_weights = F.softmax(scores, dim=-1)  # bs x 1 x 1
         output = torch.bmm(attention_weights, value).squeeze(1)  # bs x dim
+        
         return output
